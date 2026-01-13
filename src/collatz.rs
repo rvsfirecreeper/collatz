0adceaf177b4b7d3175fe8b5674f402d972826c4
Rage Fade <ragef6@tutanota.de>
2026-01-12 20:50:50 -0500

    I finished it

--- src/collatz.rs
+++ src/collatz.rs
@@ -1,8 +1,8 @@
-struct result {
-    seed: u64,
-    steps: u64,
+pub struct CollatzResult {
+    pub seed: u64,
+    pub steps: u64,
 }
-fn collatz(seed: &u64) -> result {
+pub fn collatz(seed: &u64) -> CollatzResult {
     let mut current = *seed;
     let mut steps: u64 = 0;
     let mut odd: u64;
@@ -12,12 +12,12 @@ fn collatz(seed: &u64) -> result {
     while current != 1 {
         odd = current * 3 + 1;
         even = current >> 1;
-        oddmask = -(current & 1);
-        evenmask = -(current & 1 ^ 1);
+        oddmask = (!0u64) * (current & 1);
+        evenmask = (!0u64) * (current & 1 ^ 1);
         current = (odd & oddmask) | (even & evenmask);
         steps += 1;
     }
-    result {
+    CollatzResult {
         seed: *seed,
         steps: steps,
     }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+mod collatz;
+use collatz::CollatzResult;
+use collatz::collatz;
+use std::time::Instant;
+use std::{io, u64};
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut record = CollatzResult { seed: 0, steps: 0 };
+    let mut input = String::new();
+    let max = loop {
+        println!(
+            "How many numbers would you like to calculate? Only record holders will be shown."
+        );
+        match stdin.read_line(&mut input) {
+            Ok(_) => match input.trim().parse::<u64>() {
+                Ok(n) => match n {
+                    1..=u64::MAX => break n,
+                    0 => eprintln!("Sorry, must be 1 or above."),
+                },
+                Err(_) => eprintln!("Not a number."),
+            },
+            Err(e) => eprintln!("Something went wrong! Error: {e}"),
+        }
+    };
+    println!("Starting Collatz Calculations...");
+    let start = Instant::now();
+    for i in 1..=max {
+        let current = collatz(&i);
+        if current.steps > record.steps {
+            println!(
+                "A new record! {} broke the record for most steps with {} steps!",
+                current.seed, current.steps
+            );
+            record = current;
+        }
+    }
+    println!("Calculations took just {}", start.elapsed().as_secs_f64());
 }

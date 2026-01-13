c34bebbaa7e7f384ee1513666b04517a724fe683
Rage Fade <ragef6@tutanota.de>
2026-01-13 17:15:43 -0500

    Mega commit but nothing really changed functionally aside from min and max

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 mod collatz;
 use collatz::CollatzResult;
 use collatz::collatz;
+use std::io;
+use std::io::Write;
 use std::time::Instant;
-use std::{io, u64};
+macro_rules! flush {
+    () => {
+        io::stdout().flush().unwrap();
+    };
+}
 fn main() {
+    print!(
+        "The Collatz Conjecture: Records!!\n\
+        Only records will be printed.\n"
+    );
     let stdin = io::stdin();
     let mut record = CollatzResult { seed: 0, steps: 0 };
     let mut input = String::new();
-    let max = loop {
-        println!(
-            "How many numbers would you like to calculate? Only record holders will be shown."
-        );
+    let min = loop {
+        print!("What is the first number you would like to calculate? ");
+        flush!();
         match stdin.read_line(&mut input) {
             Ok(_) => match input.trim().parse::<u64>() {
                 Ok(n) => match n {
@@ -22,9 +31,24 @@ fn main() {
             Err(e) => eprintln!("Something went wrong! Error: {e}"),
         }
     };
+    let max = loop {
+        print!("What is the last number you would like to calculate? ");
+        flush!();
+        match stdin.read_line(&mut input) {
+            Ok(_) => match input.trim().parse::<u64>() {
+                Ok(n) => break n,
+                Err(_) => eprintln!(
+                    "Not a number. Note: numbers above
+                    18,446,744,073,709,551,616
+                     are not supported"
+                ),
+            },
+            Err(e) => eprintln!("Something went wrong! Error: {e}"),
+        }
+    };
     println!("Starting Collatz Calculations...");
     let start = Instant::now();
-    for i in 1..=max {
+    for i in min..=max {
         let current = collatz(&i);
         if current.steps > record.steps {
             println!(

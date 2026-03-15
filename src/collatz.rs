c6e7a25731e2a037ee1ec2402daed640f72a5068
Rage Fade <ragef6@tutanota.de>
2026-03-15 09:28:21 -0400

    Almost twice as fast

--- src/collatz.rs
+++ src/collatz.rs
@@ -7,12 +7,14 @@ pub fn collatz(seed: &u64) -> CollatzResult {
     let mut current = *seed;
     let mut steps: u64 = 0;
     while current != 1 {
-        if current.is_multiple_of(2) {
-            current >>= 1
+        if !current.is_multiple_of(2) {
+            current = current * 3 + 1;
+            steps += (1 + current.trailing_zeros()) as u64;
+            current >>= current.trailing_zeros();
         } else {
-            current = current * 3 + 1
+            steps += current.trailing_zeros() as u64;
+            current >>= current.trailing_zeros();
         }
-        steps += 1;
     }
     CollatzResult { seed: *seed, steps }
 }
